727fff25af35fc30780027de5ac1412a2b0a929b
Martin <karag2006@gmx.net>
2024-12-08 11:20:38 +0100

    tiptap extensions

--- src/app/documents/(components)/editor.tsx
+++ src/app/documents/(components)/editor.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import Image from "@tiptap/extension-image";
 import StarterKit from "@tiptap/starter-kit";
+import ImageResize from "tiptap-extension-resize-image";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,8 +20,33 @@ export const Editor = () => {
           "flex flex-col min-h-[1054px] pt-10 pr-14 pb-10 cursor-text w-[816px] focus:outline-none print:border-0 bg-white border border-gray-300 ",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌍️</p>",
+    extensions: [
+      StarterKit,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      Image,
+      ImageResize,
+    ],
+    content: `<table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>`,
   });
 
   return (
